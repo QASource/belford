4644fed0136863069cab38b1a7045b0d800cf700
Belford <belford@qasource.com>
2012-08-07 10:56:03 +0530

    modified commonutils

--- belford_workspace/belford_automation/src/com/qa/Functions/common/CommonUtils.java
+++ belford_workspace/belford_automation/src/com/qa/Functions/common/CommonUtils.java
@@ -13,34 +13,40 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Map;
-
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
-
 public class CommonUtils {
-	public static String getDateTime() {
+	/*
+	 * This Utility function is used to find the current date and time of system.
+	 *
+	 *  
+	 */
+public static String getCurrentDateTime () {
 	    DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
 	    Date date = new Date();
 	    return dateFormat.format(date);
 	}
-	
-	public static void waitForChangesToReflect(int timeInSeconds){
+	/*
+	 * This Utility function is used to wait for the given interval.
+	 *
+	 *  
+	 */
+public static void waitForChangesToReflect(int timeInSeconds){
 		try {
 			System.out.println("System is waiting for "+ timeInSeconds+" Seconds");
 			Thread.sleep((timeInSeconds * 1000));
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-	}	
-	
+	}
 	/*
-	 * This Utility functions is used to read the data from ini file
+	 * This Utility function is used to read the data from ini file
 	 *
 	 *  
 	 */
-	public static String readIni(String str_FileName, String str_KeyName)
+public static String readIni(String str_FileName, String str_KeyName)
 	{
 		String str_FilePath;
 		str_FilePath=System.getProperty("user.dir") + "/src/com/qa/Config/" + str_FileName;
@@ -56,8 +62,12 @@ public class CommonUtils {
 		}
 		
 	}
-
-	public static void writeIni(String str_FileName, String Section, String str_KeyName, String str_Value)
+	/*
+	 * This Utility functions is used to write the String data to ini file
+	 *
+	 *  
+	 */
+public static void writeIni(String str_FileName, String Section, String str_KeyName, String str_Value)
 	{
 		String str_FilePath;
 		str_FilePath=System.getProperty("user.dir") + "/src/com/qa/Config/" + str_FileName;
@@ -66,20 +76,18 @@ public class CommonUtils {
 			Ini ini=new Ini(new FileInputStream(str_FilePath));
 			ini.put(Section, str_KeyName, str_Value);
 			ini.store(new FileOutputStream(str_FilePath));
-
-/*
-			Properties pro = new Properties();
-			pro.load(new FileInputStream(str_FilePath));
-			pro.setProperty(str_KeyName, str_Value);
-			FileOutputStream out = new FileOutputStream(str_FilePath);
-			pro.save(out, "properties updated");*/
 		}
 		catch(Exception ex)
 		{
 		}
 		
 	}
-	public static void writeIni(String str_FileName, String Section, String str_KeyName, int int_Value)
+	/*
+	 * This Utility functions is used to write the integer data to ini file
+	 *
+	 *  
+	 */
+public static void writeintIni(String str_FileName, String Section, String str_KeyName, int int_Value)
 	{
 		String str_FilePath;
 		str_FilePath=System.getProperty("user.dir") + "/src/com/qa/Config/" + str_FileName;
@@ -88,13 +96,6 @@ public class CommonUtils {
 			Ini ini=new Ini(new FileInputStream(str_FilePath));
 			ini.put(Section, str_KeyName, int_Value);
 			ini.store(new FileOutputStream(str_FilePath));
-
-/*
-			Properties pro = new Properties();
-			pro.load(new FileInputStream(str_FilePath));
-			pro.setProperty(str_KeyName, str_Value);
-			FileOutputStream out = new FileOutputStream(str_FilePath);
-			pro.save(out, "properties updated");*/
 		}
 		catch(Exception ex)
 		{
@@ -102,11 +103,15 @@ public class CommonUtils {
 		
 		
 	}
-	public static int toInt(String str_String)
+	/*
+	 * This Utility functions is used to  convert the String to int
+	 *
+	 *  
+	 */
+public static int toInt(String str_String)
 	{
 	    try
 	    {
-	      // the String to int conversion happens here
 	      int i = Integer.parseInt(str_String.trim());
 	      return(i);
 	    }
@@ -116,39 +121,48 @@ public class CommonUtils {
 	    }
 	    return(0);
 	}
-	
-	/*public static void updateBuildParameter()
+	/*
+	 * This Utility functions is used to update the build parameter
+	 *
+	 *  
+	 */	
+public static void updateBuildParameter()
 	{
 		int value = toInt(readIni("Environment.ini", "Build_Number"));
 		value=value+1;
-		writeIni("Environment.ini", "Build_Number", value);
-		commitFile();	
+		writeintIni("Environment.ini","", "Build_Number", value);
+//		commitFile();	
 		
-	}*/
-	
-	public static int generateRandomNumber()
+	}
+	/*
+	 * This Utility functions is used to generate a random number
+	 *
+	 *  
+	 */	
+public static int generateRandomNumber()
 	{
 		Random rand = new Random();
 		return rand.nextInt(10000);
 
 	}
-	public static void UpdateConfigAppNo()
+	/*
+	 * This Utility functions is used to update the application number
+	 *
+	 *  
+	 */	
+public static void UpdateConfigAppNo()
 	{
 		int value = toInt(readIni("TestData.ini", "Config_App_Number"));
 		value=value+1;
-		writeIni("TestData.ini", "Others", "Config_App_Number", value);
+		writeintIni("TestData.ini", "Others", "Config_App_Number", value);
 		System.out.println("Application number is updated");
 	}
-	
-	
 	/**
-	 * report test execution to TestLink API
-	 * 
-	 * @param int tcid
-	 * @param int tpid
-	 * @param String status
-	 */
-	public static void tcmsReport(int tcid, int tpid, String status)
+	 * This function Changes the status of the test cases to Passed/Failed
+	 * @param testcaseid  Test case id of the test case.
+	 * @param status marks the status of testcase ID as Pass/Fail/
+	 **/
+	public static void tcmsReport(String testcaseid, String status)
 	{
 		try 
 		{
@@ -163,20 +177,22 @@ public class CommonUtils {
 			ArrayList<Object> params = new ArrayList<Object>();
 			Hashtable<String, Object> executionData = new Hashtable<String, Object>();				
 			executionData.put("devKey", CommonUtils.readIni("Environment.ini", "str_DevKey"));
-			executionData.put("tcid", tcid);
-			executionData.put("tpid", tpid);
+			executionData.put("testcaseexternalid", testcaseid);
+			executionData.put("testplanid", CommonUtils.readIni("Environment.ini", "str_TestPlanId"));
 			executionData.put("status", status);
+			executionData.put("buildname", CommonUtils.readIni("Environment.ini", "str_BuildName"));
+			executionData.put("overwrite", true);
 			params.add(executionData);
 			
 			Object[] result = (Object[]) rpcClient.execute("tl.reportTCResult", params);
+	        
+			System.out.println("Result was:\n");            
+	         for (int i=0; i< result.length; i++)
+	         {
+	            Map item = (Map)result[i];
+	            System.out.println("Keys: " + item.keySet().toString() + " values: " + item.values().toString());
+	         }
 
-			// Typically you'd want to validate the result here and probably do something more useful with it
-			System.out.println("Result was:\n");				
-			for (int i=0; i< result.length; i++)
-			{
-				Map item = (Map)result[i];
-				System.out.println("Keys: " + item.keySet().toString() + " values: " + item.values().toString());
-			}
 		}
 		catch (MalformedURLException e)
 		{
